7d02ddf9afe015f15e4c5935768e6c746a0fbf1b
Mohammad Ayub <5043ayub@gmail.com>
2025-01-16 18:53:49 +0600

    First commit of layout, router dom and tailwind setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Layout from "../Layout/Layout";
+
+const Router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Layout></Layout>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
+  }
+]);
+
+export default Router;
